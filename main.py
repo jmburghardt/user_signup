9f86c44e33446934083a7552e39818c149d54925
Joe Burghardt <jmburghardt@gmail.com>
2018-04-08 12:58:39 -0500

    functioning, need welcome page

--- main.py
+++ main.py
@@ -6,39 +6,51 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/signup', methods=['post'])
+def signup():
+    username = get_username()
+    password = get_password()
+    verify = validate_password()
+    email = get_email()
+    return render_template('options.html', value=username[0], error=username[1], 
+        valuepass=password[0], errorpass=password[1], errorverify=verify, erroremail=email)
+
 def get_username():
     username = request.form['username']
     if (not username) or (username.strip() == ''):
         error = "That's not a valid username"
-        return render_template('options.html', error=error)
+        return  ('', error)
 
     if len(username) < 3 or len(username) > 20:
         error = "Your username must be between 3 and 20 characters."
-        return render_template('options.html', error=error)
+        return ('', error)
     else:
-        return render_template('options.html', value=username)
+        return (username, '')
 
 def get_password():
     password = request.form['password']
     if (not password) or (password.strip() == ''):
         error = "That's not a valid password"
-        return render_template('options.html', error=error)
+        return ('', error)
     if len(password) < 3 or len(password) > 20:
         error = "Your password must be between 3 and 20 characters"
-        return render_template('options.html', error=error)
+        return ('', error)
+    else:
+        return (password, '')
 
 def validate_password():
     verify = request.form['verify']
     password = request.form['password']
     if verify != password:
         error = "Passwords don't match"
-        return render_template('options.html', error=error)
+        return (error)
 
-def email():
+def get_email():
     email = request.form['email']
-    if not email:
-        error = "That's not a valid email"
-        return render_template('options.html', error=error)
+    error = "That's not a valid email"
+    if "@" in email and "." in email:
+        return ""
+    else:
+        return error
 
 @app.route('/')
 def index():
